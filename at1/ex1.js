9f92104ffcbaf74d906eb69a2997e5c3aa307fe8
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-03-08 09:04:55 -0300

    Atv 1
    Formas de linkar javascript e html, com exemplos

---
+++ at1/ex1.js
@@ -0,0 +1,9 @@
+function Logar() {
+    let Email = document.getElementById("email").value
+   
+    if (Email===""){
+        alert("Todos os campos devem ser preenchidos")
+        return false;
+    }
+
+}
